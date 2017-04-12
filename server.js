8cb4f18c8dc1af154e46a0875c9e06f584464391
Natasha Che <natasha@natashache.com>
2017-04-12 15:00:56 -0400

    fixed prerender

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ var path = require('path')
 var app = express()
 app.use(bodyParser.json())
 
-// app.use(function(req, res, next) {
-//   console.log('before prerender-node');
-//   next();
-// })
-
 // app.use(require('prerender-node').set('prerenderToken', 'XJx822Y4hyTUV1mn6z9k'))
 
 app.use(require('prerendercloud')).set('prerenderToken', 'dXMtd2VzdC0yOjE2MDE0OTIyLTk5MTgtNGY1Yi1hOTQwLTY1MDI2MzYyYTRlNQ.dE2HiZLJmqwNG0aJsAcWqmZHt_iAsV2tcIQQbvs2zPI')
