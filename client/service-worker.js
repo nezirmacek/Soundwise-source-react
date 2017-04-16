bcb8c50fdef54f1c7fdcb9816e72924e8f5f50b8
Natasha Che <natasha@natashache.com>
2017-04-16 15:14:44 -0400

    changed service worker

--- client/service-worker.js
+++ client/service-worker.js
@@ -227,7 +227,7 @@ self.addEventListener('fetch', function(event) {
 
     // If shouldRespond is still false, check to see if this is a navigation
     // request, and if so, whether the URL matches navigateFallbackWhitelist.
-    var navigateFallback = '';
+    var navigateFallback = 'index.html';
     if (!shouldRespond &&
         navigateFallback &&
         (event.request.mode === 'navigate') &&
