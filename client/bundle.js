088dbba7f995af866744facfc45b4c8832294186
Natasha Che <natasha@natashache.com>
2017-05-29 10:23:11 -0400

    course change

--- client/bundle.js
+++ client/bundle.js
@@ -8303,199 +8303,115 @@
 	// const history = syncHistoryWithStore(browserHistory, store)
 
 	var courseInfo = {
-	  "name": "Raising Venture Capital: What You Need to Know",
-	  "img_url_mobile": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/venture_capital.png",
-	  "id": 121,
-	  "run_time": 3386,
+	  "name": "How to Find the Right Business Model for Your Startup",
+	  "img_url_mobile": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/How+to+Find+the+Right+Business+Model.png",
+	  "id": 124,
+	  "run_time": 2625,
 	  "price": 0,
-	  "trailer_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/1+Intro+_mixdown.mp3",
+	  "trailer_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-1.mp3",
 	  "category": "Entrepreneurship",
-	  "keywords": "entrepreneurship, entrepreneur, Stephen Pearse, venture capital, raise money, financing, pitch, startup pitch, pitch deck, equity financing, venture funding, MIT, MIT Sloan, startups, marketing, product launch, sales, customer acquisition, lean startup, business, audiobooks, audio courses, online courses",
-	  "description": ["Need to pitch your startup to venture capitalists? This course tells you how. Stephen Pearse, a serial entrepreneur and startup veteran, teaches you the nitty-gritties of pitching and presenting that will help you maximize your chance of success with potential investors."],
-	  "description_long": ["As an entrepreneur, you always need to be selling your ideas—to potential employees, customers, partners, and investors. How quickly your business can gain traction highly depends on how good you are in making others buy into your vision.", "But how do you position and present your idea in the best light?", "Drawing on 30 years of experience as an entrepreneur, senior executive and venture capitalist, in this course Stephen Pearse will help you get into the minds of your potential investors, and show you how to make your pitch stand out in a sea of noise.", "Although this course focuses on pitching venture capital investors, the insights and practical tips shared will help you become a better salesperson for your ideas, no matter whom you are pitching to."],
-	  "features": ["Learn the right way to pitch your ideas that gets results", "Tips on how to approach venture investors", "Presentation techniques that capture your audience", "Invaluable advices from a seasoned entrepreneur", "12 audio lessons", "Infographic notes for individual lessons", "Assignments/action steps for each lesson", "Option to play audios offline (on computer and android phone with Chrome browser"],
+	  "keywords": "entrepreneurship, entrepreneur, Rich Kivel, business model, business plan, revenue model, MIT, MIT Sloan, startups, marketing, product launch, sales, customer acquisition, lean startup, business, audiobooks, audio courses, online courses",
+	  "description": ["In this example-rich course, Richard Kivel will help you answer the million-dollar question: “How do your startup make money?” The course explores some of the common business models and discusses when they are mostly appropriately used."],
+	  "description_long": ["To build a successful startup, having a killer product is not enough. You need to figure out how you will get your product into the hands of your customers and effectively capture part of the value you create. And as technologies evolve, the way you distribute your project and make money may evolve as well.", "In this course, serial entrepreneur and investor Rich Kivel will teach you how to go from having a business idea to ideally positioning yourself in your industry and product category, by choosing the right business model."],
+	  "features": ["Learn the characteristics of successful business models", "Tips on how to find your ideal market positioning", "Insights for identifying market opportunites in your industry", "Business advices from a seasoned entrepreneur", "6 audio lessons", "Action steps for every lesson", "Option to play audios offline (on computer and android phone with Chrome browser"],
 	  "metrics": {
-	    40: {
+	    52: {
 	      timesCompleted: 0
 	    },
-	    41: {
+	    53: {
 	      timesCompleted: 0
 	    },
-	    42: {
+	    54: {
 	      timesCompleted: 0
 	    },
-	    43: {
+	    55: {
 	      timesCompleted: 0
 	    },
-	    44: {
+	    56: {
 	      timesCompleted: 0
 	    },
-	    45: {
-	      timesCompleted: 0
-	    },
-	    46: {
-	      timesCompleted: 0
-	    },
-	    47: {
-	      timesCompleted: 0
-	    },
-	    48: {
-	      timesCompleted: 0
-	    },
-	    49: {
-	      timesCompleted: 0
-	    },
-	    50: {
-	      timesCompleted: 0
-	    },
-	    51: {
+	    57: {
 	      timesCompleted: 0
 	    }
 	  },
-	  "teacher": "Stephen Pearse",
-	  "teacher_profession": "Entrepreneur and Venture Capitalist",
-	  "teacher_img": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/StephenPearse.jpg",
-	  "teacher_thumbnail": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/StephenPearse.jpg",
-	  "teacher_bio": ["Steven Pearse has been a technology leader and entrepreneur for 30 years. He is a Managing Director at Yucatan Rock Ventures. He has been the CEO of numerous startups in the telecommunications space, most notably Cyras Systems. Before starting his entrepreneurial career, He was Executive Vice President of Bay Networks' Internet/Telecom Business Group. Previously, he served as Senior Vice President at Time Warner Telecom and Vice President of Technology at Sprint. He is the Chairman of Innovation Pavilion, a Director of Sereniti, Inc., Voltree, Mobiwatch and C4RJ, and is a Techstars mentor and NASA advisory council appointee."],
-	  "teacher_website": "https://en.wikipedia.org/wiki/Stephen_Pearse",
-	  "teacher_linkedin": "https://www.linkedin.com/in/pearse/",
+	  "teacher": "Richard Kivel",
+	  "teacher_profession": "Entrepreneur and Investor",
+	  "teacher_img": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/Richard_Kivel.jpeg",
+	  "teacher_thumbnail": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/Richard_Kivel.jpeg",
+	  "teacher_bio": ["Rich Kivel has over 20 years of experience in managing and building successful emerging technology companies, from founding and raising capital to positioning the companies for acquisition. He is the Executive Chairman of ViS Research Institute, Inc. and a Senior Manager at Bridgewater Associates. He is a frequent Guest Lecturer at MIT Sloan School of Management and has been a Judge for the MIT $100k Entrepreneurship Competition since 1998."],
+	  "teacher_website": "",
+	  "teacher_linkedin": "https://www.linkedin.com/in/kivel/",
 	  "teacher_facebook": "",
-	  "teacher_twitter": "https://twitter.com/sgpearse",
+	  "teacher_twitter": "https://twitter.com/kivel1",
 	  "teacher_instagram": "",
 	  "modules": [{
 	    "module_id": 1,
-	    "module_title": "Raising Venture Capital: What You Need to Know",
+	    "module_title": "How to Find the Right Business Model for Your Startup",
 	    "sections": [{
-	      "section_id": 40,
+	      "section_id": 52,
 	      "section_number": 1,
-	      "run_time": "1:50",
-	      "title": "Introduction",
+	      "run_time": "7:52",
+	      "title": "Why the business model can make or break your business",
+	      "content": "",
+	      "actions": "Think about your answers to the following questions. As technologies progress, how has that changed the ways goods and services are delivered to consumers? How will the emerging technologies being developed today affect your company’s business model five years from now?",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-1.mp3",
+	      "transcript_url": "",
+	      "notes_url": "",
+	      "resources": ""
+	    }, {
+	      "section_id": 53,
+	      "section_number": 2,
+	      "run_time": "3:39",
+	      "title": "The one crucial characteristic of any successful business model",
+	      "content": "",
+	      "actions": "Brainstorm and come up with at least five approaches of market distribution that can get your company’s product into the hands of your target customers.",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-2.mp3",
+	      "transcript_url": "",
+	      "notes_url": "",
+	      "resources": ""
+	    }, {
+	      "section_id": 54,
+	      "section_number": 3,
+	      "run_time": "13:41",
+	      "title": "The seven elements of a winning business model",
+	      "content": "",
+	      "actions": "Write down your answers to the following questions. 1. What’s your company’s value proposition? 2. Who are your target customers? 3. Where are you in your industry’s value chain? 4. What are your cost structure and target profit margins? 5. How will you develop a sustainable competitive advantage?",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-3.mp3",
+	      "transcript_url": "",
+	      "notes_url": "",
+	      "resources": [{ "link": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/businessmodelgeneration_preview.pdf", "description": "Useful book on busines model generation", "link_text": "Business Model Generation by Alexander Osterwalder and Yves Pigneur" }, { "link": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/business_model_canvas_poster.pdf", "description": "Business model canvas", "link_text": "Canvas template" }]
+	    }, {
+	      "section_id": 55,
+	      "section_number": 4,
+	      "run_time": "6:08",
+	      "title": "The evolution of business model in action-- Google",
+	      "content": "",
+	      "actions": "The cost of digital storage has dropped dramatically over the past twenty years. How has that changed the business models of existing and new companies? Think of at least three examples.",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-4.mp3",
+	      "transcript_url": "",
+	      "notes_url": "",
+	      "resources": ""
+	    }, {
+	      "section_id": 56,
+	      "section_number": 5,
+	      "run_time": "11:41",
+	      "title": "Examples of successful business models and what you can learn from them",
+	      "content": "",
+	      "actions": "Can you think of anything in common among the business model examples mentioned in this lesson? Hint: Think of how those companies take advantage of new technological progress to reinvent old industries.",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-5.mp3",
+	      "transcript_url": "",
+	      "notes_url": "",
+	      "resources": ""
+	    }, {
+	      "section_id": 57,
+	      "section_number": 6,
+	      "run_time": "0:44",
+	      "title": "Summary",
 	      "content": "",
 	      "actions": "",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/bob_jones/1+Intro_mixdown.mp3",
+	      "section_url": "https://s3.amazonaws.com/soundwiseinc/rich_kivel/lesson-6.mp3",
 	      "transcript_url": "",
 	      "notes_url": "",
-	      "resources": ""
-	    }, {
-	      "section_id": 41,
-	      "section_number": 2,
-	      "run_time": "3:49",
-	      "title": "What your investors want",
-	      "content": "",
-	      "actions": "Ask yourself the following questions. What is the long term vision for your company? What is your time horizon to achieve that vision? And how can your vision be consistent with the goals your potential investors may have?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/2+VC+biz+model_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-2.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 42,
-	      "section_number": 3,
-	      "run_time": "3:37",
-	      "title": "Your advantage as a startup over big companies",
-	      "content": "",
-	      "actions": "Think about the following questions. Which larger companies in your industry can potentially acquire your startup? What advantages does your startup have over other bigger companies in your industry? What can you do that they cannot?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/3+The+startup+advantage+over+big+companies+mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "",
-	      "resources": ""
-	    }, {
-	      "section_id": 43,
-	      "section_number": 4,
-	      "run_time": "4:41",
-	      "title": "The fundamentals of a good pitch",
-	      "content": "",
-	      "actions": "Draft an elevator pitch for your startup that include the following elements: 1. the target customers; 2. the customer problem; 3. the current dissatisfactory solutions available; 4. the solution you provide; 5. what set you apart from competitive substitutes; 6. the key features of your solution. For example, your pitch can go like this: For so and so target customers, who have such and such problems and are not satisfied with such and such current solutions, our product is a…fill in new product category…that provides such and such a solution. Unlike such and such competitive substitutes, we have such and such key product features. Deliver this pitch to at least ten of your friends and families, and take feedback.",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/4+The+basics+of+a+good+pitch_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-4.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 44,
-	      "section_number": 5,
-	      "run_time": "6:22",
-	      "title": "Eight essential rules of a good pitch",
-	      "content": "",
-	      "actions": "Draft a pitch presentation for your startup following the 10-slides, 20-minutes, 30 pixel-font size rule. Think from your audience’s perspective. Is your presentation simple enough? Does it have an exciting hook? What questions may your audience have?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/5+Eight+rules+of+a+good+pitch+_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-5.jpg",
-	      "resources": [{ "link": "https://guykawasaki.com/the_102030_rule/", "description": "Guy Kawasaki's blog", "link_text": "The 10/20/30 Rule of PowerPoint" }, { "link": "https://www.youtube.com/watch?v=jJ2yepIaAtE", "description": "The Pecha Kucha presentation style", "link_text": "A Pecha Kucha about Pecha Kucha" }]
-	    }, {
-	      "section_id": 45,
-	      "section_number": 6,
-	      "run_time": "4:59",
-	      "title": "The anatomy of a good pitch",
-	      "content": "",
-	      "actions": "Review your pitch presentation. Does it include the following ten components? 1. The hook. 2. Your solution to the customer problem. 3. Your magic and/or your technology. 4. Your business model. 5. Your marketing and sales strategy. 6. Your competition. 7. Your management team. 8. How you’re going to spend the investment money. 9. Your current status and timeline. 10. What your request to the potential investors is.",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/6+The+anatomy+of+a+good+pitch_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-6.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 46,
-	      "section_number": 7,
-	      "run_time": "4:09",
-	      "title": "How to get investors excited about your pitch",
-	      "content": "",
-	      "actions": "Review your pitch presentation again. Does it have an exciting hook, for example, addressing a significant pain or implying a huge market opportunity? Is your presentation layman-friendly? Is your presentation sufficiently focused on the customer benefits of your product?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/7+How+to+get+investors+mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-7.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 47,
-	      "section_number": 8,
-	      "run_time": "3:03",
-	      "title": "Six presentation tips for a winning pitch",
-	      "content": "",
-	      "actions": "Practice your pitch presentation to at least five friends, families, or mentors, and ask for feedback.",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/8+Six+presentation+tips+to+help+you+deliver+a+winning+pitch_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-8.jpg",
-	      "resources": [{ "link": "https://www.inc.com/magazine/201310/leigh-buchanan/pechakucha-a-peppy-brief-presentation-style.html", "description": "How to Add More Pep to Your Presentations, by Leigh Buchanan", "link_text": "How to Add More Pep to Your Presentations" }]
-	    }, {
-	      "section_id": 48,
-	      "section_number": 9,
-	      "run_time": "7:50",
-	      "title": "Good and bad examples of real pitches",
-	      "content": "",
-	      "actions": "Google and find five pitch decks of other companies. Review them and think about: 1) What do you like and not like about them? 2) How can they be improved?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/9+Good+and+bad+examples+of+actual+pitches_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "",
-	      "resources": ""
-	    }, {
-	      "section_id": 49,
-	      "section_number": 10,
-	      "run_time": "5:37",
-	      "title": "When and how to approach investors",
-	      "content": "",
-	      "actions": "Research the background and identify at least five venture capital firms that could be a good fit for your startup. Find out who’s who in these companies and look for common connections. Is there anyone in your personal or professional network who can introduce you to these VCs? Can you think of other creative ways to get an introduction?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/10+When+and+how+you+should+approach+investors_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-10.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 50,
-	      "section_number": 11,
-	      "run_time": "2:52",
-	      "title": "Two crucial metrics to watch for in your startup",
-	      "content": "",
-	      "actions": "Ask yourself, am I paying enough attention to my startup’s cashflow? Think of ways you can better monitor your company’s financials.",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/11+Two+crucial+factors+to+watch+for+to+ensure+you+company's+successmixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-10.jpg",
-	      "resources": ""
-	    }, {
-	      "section_id": 51,
-	      "section_number": 12,
-	      "run_time": "7:37",
-	      "title": "More advices on how to ensure your company's long-term success",
-	      "content": "",
-	      "actions": "Ask yourself the following questions. What are the unique competitive advantages of your company over your competitors? Why are you and your team the right people building this specific company? What can you do to make up for the skill and experience gap you may have? And what is your company’s larger mission besides making a profit?",
-	      "section_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/12+Even+more+tips+for+ensuring+your+company's+long+term+success_mixdown.mp3",
-	      "transcript_url": "",
-	      "notes_url": "https://s3.amazonaws.com/soundwiseinc/stephen_pearse/lesson-12.jpg",
 	      "resources": ""
 	    }]
 	  }]
