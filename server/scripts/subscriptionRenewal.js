0674420f588d1d85ec801c67d6b21b7af2da81de
Natasha Che <natasha@natashache.com>
2017-09-04 17:11:05 -0400

    fixed subscription renewal webhook

--- server/scripts/subscriptionRenewal.js
+++ server/scripts/subscriptionRenewal.js
@@ -3,20 +3,20 @@
 var admin = require("firebase-admin");
 
 module.exports.subscriptionRenewal = (req, res) => {
-  console.log('req.data: ', req.data);
+  // console.log('req.body: ', req.data);
 
-  if (req.data.object.lines.data[0].period.end) {
-    const customer = req.data.object.customer;
+  if (req.body.data.object.lines.data[0].period.end) {
+    const customer = req.body.data.object.customer;
     const db = admin.database();
     const ref = db.ref('users');
     ref.orderByChild('stripe_id').equalTo(customer)
       .on('value', snapshot => {
         const userId = snapshot.key;
-        db.ref(`users/${userId}/soundcasts`).orderByChild('planID').equalTo(req.data.object.lines.data[0].plan.id)
+        db.ref(`users/${userId}/soundcasts`).orderByChild('planID').equalTo(req.body.data.object.lines.data[0].plan.id)
           .on('value', snapshot => {
             const soundcast = snapshot.key;
             db.ref(`users/${userId}/soundcasts/${soundcast}/current_period_end`)
-              .set(req.data.object.lines.data[0].period.end);
+              .set(req.body.data.object.lines.data[0].period.end);
           });
       });
     res.status(200).send({});
