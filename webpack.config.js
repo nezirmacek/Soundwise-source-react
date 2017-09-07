b1239eef8c8fe39ba6c16835dce379ac8e3a86a2
Natasha Che <natasha@natashache.com>
2017-09-07 17:08:22 -0400

    added one more route

--- webpack.config.js
+++ webpack.config.js
@@ -94,11 +94,11 @@ module.exports = {
   //       // }],
   //     }
   //   ),
-    // new webpack.optimize.UglifyJsPlugin(),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify('production')
-    //   }
-    // })
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
   ]
 };
