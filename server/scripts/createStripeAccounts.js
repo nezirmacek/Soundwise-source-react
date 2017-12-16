b4d04f9768c329df796f2b7bb8570bdbd4ec0503
Natasha Che <natasha@natashache.com>
2017-12-16 18:34:00 -0500

    fixed small error

--- server/scripts/createStripeAccounts.js
+++ server/scripts/createStripeAccounts.js
@@ -1,5 +1,6 @@
 'use strict';
 var stripe_key =  require('../../config').stripe_key;
+var stripe = require('stripe')(stripe_key);
 var request = require('request-promise');
 var firebase = require('firebase-admin');
 
