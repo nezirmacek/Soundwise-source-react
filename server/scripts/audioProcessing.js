47bcb7fbdebc65a49367c7e4d4b0641d88e02503
IvanM <ivan.malyshev.376@gmail.com>
2018-03-23 16:38:12 +0300

    add ffmpeg

--- server/scripts/audioProcessing.js
+++ server/scripts/audioProcessing.js
@@ -1,18 +1,86 @@
+'use strict';
+const firebase = require('firebase-admin');
+const request = require('request-promise');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(require('../../config').sendGridApiKey);
+const fs = require('fs');
+const ffmpeg = require('./ffmpeg');
 
 module.exports.audioProcessing = async (req, res) => {
 	// 1. Client make post request to /api/audio_processing, with episode ID and processing options
+	// request example: {
+		// epsiodeId: '1519588329916e', // need to retrieve episode info from firebase for tagging
+		// soundcastId: '1508293913676s', // need to retrieve soundcast info from firebase for tagging
+		// publisherEmail: 'john@awesomepublisher.com',
+		// publisherFirstName: 'John',
+		// tagging: true,
+		// intro: 'https://mysoundwise.com/tracks/intro12345s.mp3',
+		// outro: 'https://mysoundwise.com/tracks/outro12345s.mp3',
+		// overlayDuration: 2, // how many seconds intro/outro should overlay the main audio,
+		// setVolume: true,
+		// trim: true,
+		// removeSilence: 0.7, // the number is the silence remove threshold. e.g. remove silence longer than 0.7 second
+		// autoPublish: false, // if true, automatically publish the episode after processing, otherwise, save the episode as draft and notify user by email
+		// emailListeners: false }
 	const { epsiodeId, soundcastId, publisherEmail, publisherFirstName,
 					tagging, intro, outro, overlayDuration, setVolume, trim,
 					removeSilence, autoPublish, emailListeners } = req.body;
 	if (epsiodeId && soundcastId && publisherEmail && publisherFirstName &&
-			tagging && intro && outro && overlayDuration && setVolume && trim &&
-			removeSilence && autoPublish && emailListeners) {
+			typeof tagging         === 'boolean' && intro && outro &&
+			typeof overlayDuration === 'number'  && typeof setVolume      === 'boolean' &&
+			typeof trim            === 'boolean' && typeof removeSilence  === 'number'  &&
+			typeof autoPublish     === 'boolean' && typeof emailListeners === 'boolean') {
 	  // 2. return 200 ok to client if request body includes all necessary information
 		res.end('ok');
 
 		// 3. Get the episode url from firebase 'episodes/[episode id]/url', fetch the audio file
-
+		const episode = await firebase.database().ref(`episodes/${epsiodeId}`).once('value');
+		const episodeVal = episode.val();
+		request.get({
+			url: episodeVal.url,
+			encoding: null // return body as a Buffer
+		}).then(async body => {
+			const filePath = `/tmp/audio_processing_${episode.id + episode.url.slice(-4)}`;
+			fs.writeFile(filePath, body, err => {
+				if (err) {
+					return console.log(`Error: audio processing cannot write tmp audio file ${filePath}`);
+				}
+				// 4. Run some or all of the following processing options, depending on what options are requested from the client:
+				//    A: [tagging] add metadata tags to the episode audio file
+				//    B: [trim] trim off silence at beginning and end of audio file
+				//    C: [intro, outro] attach intro/outro clips to audio file
+				//    D: [setVolume] harmonize volume and set loudness to desired level
+				//    E: [removeSilence] remove excessively long silence throughout the audio file (e.g. any silence longer than 0.7 second)
+				try {
+					(new ffmpeg(filePath)).then(file => {
+						if (tagging) {
+							
+						}
+						if (trim) {
+							
+						}
+						if (intro) {
+							
+						}
+						if (outro) {
+							
+						}
+						if (setVolume) {
+							
+						}
+						if (removeSilence) {
+							
+						}
+					}, err => console.log(`Error: audio processing unable to parse file with ffmpeg ${err}`));
+				} catch(e) {
+					console.log(`Error: audio processing ffmpeg catch ${e.body || e.stack}`);
+				}
+			}); // fs.writeFile
+		}).catch(err => {
+	    console.log(`Error: audio processing unable to obtain episode ${err}`);
+	    // res.error(`Error: unable to obtain episode ${err}`);
+	  });
 	} else {
-		res.error('Error: undefined parameters');
+		res.error('Error: audio processing undefined parameters');
 	}
 }
