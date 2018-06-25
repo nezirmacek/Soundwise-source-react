0ef1f3f031f05eb0b91114cccac81a78399178e8
Natasha Che <natasha@natashache.com>
2018-06-25 16:39:03 -0400

    fixed blog header

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
         videojs: 'video.js', 'window.videojs': 'video.js',
         RecordRTC: 'recordrtc', 'window.RecordRTC': 'recordrtc',
     }),
-    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
