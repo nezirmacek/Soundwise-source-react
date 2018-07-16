26d94044caa50e4e4664835b19949d082b94f2f1
Natasha Che <natasha@natashache.com>
2018-07-16 09:13:16 -0400

    merging

---
+++ client/components/podcasting_course_landing_page.js
@@ -0,0 +1,115 @@
+
+            <section class="position-relative cover-background hero-style19 tz-builder-bg-image border-none xs-padding-50px-tb hero-style2" id="hero-section17" style="background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(&quot;images/bg-image/hero-bg11.jpg&quot;);" data-selector=".tz-builder-bg-image">
+                <div class="container one-fourth-screen position-relative">
+                    <div class="row">
+                        <div class="slider-typography xs-position-static text-center">
+                            <div class="slider-text-middle-main">
+                                <div class="slider-text-middle text-center xs-padding-fifteen xs-no-padding-lr">
+                                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 center-col">
+                                        <h1 class="title-extra-large-4 line-height-65 font-weight-500 sm-title-extra-large-4 xs-title-extra-large-2 text-white alt-font margin-eight-bottom tz-text" style="" data-selector=".tz-text">Create A Profitable Podcast That Radically Increases Your Sales, Conversions, And Customer Loyalty In 8 Weeks</h1>
+                                        <div class="text-light-gray text-extra-large xs-text-extra-large width-80 md-width-100 margin-twelve-bottom tz-text margin-lr-auto" data-selector=".tz-text" style="">Without the overwhelms, tech headaches, and uncomfortable sales pitches. Even if you are not a “good speaker” and know nothing about podcasting.</div>
+                                        <div class="btn-dual">
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="padding-110px-tb xs-padding-60px-tb bg-white builder-bg" id="content-section32" data-selector=".builder-bg" style="">
+                <div class="container">
+                    <div class="row equalize xs-equalize-auto equalize-display-inherit">
+                        <div class="col-lg-5 col-md-6 col-sm-6 xs-12 xs-text-center xs-margin-nineteen-bottom display-table" style="height: 651px;">
+                            <div class="display-table-cell-vertical-middle">
+
+                                <!-- section title -->
+                                <h2 class="alt-font title-extra-large sm-title-large xs-title-large text-fast-blue margin-eight-bottom tz-text sm-margin-ten-bottom" data-selector=".tz-text" style="">Stop the customer acquisition treadmill</h2>
+                                <!-- end section title -->
+                                <div class="text-medium tz-text width-90 sm-width-100 margin-seven-bottom sm-margin-ten-bottom" style="" data-selector=".tz-text">If you are a coach, consultant, service provider, or online entrepreneur, you know how hard it is to get your message out there these days. It’s a noisy world we live in. Most hardworking business owners struggle because they spend so much time working on things that have a low return-- feeding their social media accounts with low-conversion content and calling it “marketing”, or cold calling prospects and running after individual leads that don’t amount to must most of the time. </div>
+                                <div class="text-medium tz-text width-90 sm-width-100 margin-fifteen-bottom sm-margin-ten-bottom" style="" data-selector=".tz-text">You may have had the sad realization that the endless social media treadmill-running hardly moves the needle for your business. And trying to hunt down individual prospects and prove yourself to each and every one of them is absolutely exhausting. </div>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-6 col-sm-6 xs-12 xs-text-center display-table" style="height: 651px;">
+                            <div class="display-table-cell-vertical-middle">
+                                <img alt="" src="images/application-screenshot-img-06.png" data-img-size="(W)800px X (H)785px" data-selector="img" style="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="padding-110px-tb xs-padding-60px-tb bg-white builder-bg border-none" id="title-section1" data-selector=".builder-bg" style="">
+                <div class="container">
+                    <div class="row">
+                        <!-- section title -->
+                        <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                            <h2 class="section-title-large sm-section-title-medium text-dark-gray font-weight-700 alt-font margin-three-bottom xs-margin-fifteen-bottom tz-text" style="" data-selector=".tz-text">What if you can have a reliable way to spread your message that regularly brings in additional revenue every month…while establishing yourself as an industry leader and subject-matter expert?</h2>
+
+                        </div>
+                        <!-- end section title -->
+                    </div>
+                </div>
+            </section>
+            <section class="padding-110px-tb xs-padding-60px-tb bg-white builder-bg" id="content-section50" data-selector=".builder-bg" style="">
+                <div class="container">
+                    <div class="row equalize xs-equalize-auto equalize-display-inherit">
+                        <div class="col-md-6 col-sm-6 xs-12 xs-text-center display-table" style="height: 481px;">
+                            <div class="display-table-cell-vertical-middle">
+                                <img alt="" src="images/content-50.jpg" data-img-size="(W)800px X (H)681px" data-selector="img" style="">
+                            </div>
+                        </div>
+                        <!-- content details -->
+                        <div class="col-md-6 col-sm-6 xs-12 xs-text-center xs-margin-nineteen-bottom display-table" style="height: 481px;">
+                            <div class="display-table-cell-vertical-middle">
+
+                                <div class="text-extra-large tz-text width-90 sm-width-100 margin-five-bottom sm-margin-ten-bottom" style="" data-selector=".tz-text">In case you’ve been living under a rock for the past three years, podcasting has been growing by leaps and bounds. The number of podcast listeners in the US is increasing at about 40% a year. There are now more podcast listeners in the country than Twitter users. </div>
+                                <div class="text-medium tz-text width-90 sm-width-100 margin-ten-bottom sm-margin-ten-bottom xs-margin-twenty-bottom" style="" data-selector=".tz-text"><p>Businesses of all sizes and industries, from your local hardware store to large enterprises like Goldman Sachs and McKinsey &amp; Company, are starting their own podcast to generate new leads, market their brand, and convert more customers.</p><p><span id="docs-internal-guid-178f6e5a-9ace-8835-80a4-554e22d2ffe1"><span style="font-size: 11pt; font-family: Arial; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">Why? Because-- </span></span>
+</p></div>
+
+                            </div>
+                        </div>
+                        <!-- end content details -->
+                    </div>
+                </div>
+            </section>
+            <section class="padding-110px-tb xs-padding-60px-tb bg-white builder-bg border-none" id="title-section1" data-selector=".builder-bg" style="">
+                <div class="container">
+                    <div class="row">
+                        <!-- section title -->
+                        <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                            <h2 class="section-title-large sm-section-title-medium text-dark-gray font-weight-700 alt-font margin-three-bottom xs-margin-fifteen-bottom tz-text" style="" data-selector=".tz-text">PODCASTING IS THE NEW CUSTOMER CONVERSION SECRET WEAPON</h2>
+                            <div class="text-medium width-60 margin-lr-auto md-width-70 sm-width-100 tz-text" style="" data-selector=".tz-text">Even if you’ve had your own blog/vlog or been able to get press coverage, having your own podcast can help you:</div>
+                        </div>
+                        <!-- end section title -->
+                    </div>
+                </div>
+            </section>
+            <section class="padding-110px-tb bg-white builder-bg xs-padding-60px-tb" id="title-section8" data-selector=".builder-bg" style="">
+                <div class="container">
+                    <div class="row four-column">
+                        <div class="col-md-3 col-sm-6 col-xs-12 sm-margin-ten-bottom xs-margin-twenty-three-bottom">
+                            <h5 class="margin-eight-bottom text-dark-sky-blue text-large alt-font sm-title-small xs-margin-seven-bottom tz-text" style="" data-selector=".tz-text">Get more customers to buy from you</h5>
+                            <div class="text-medium tz-text" style="" data-selector=".tz-text"><p>Your podcast is an amazing tool for showcasing your expertise, building your brand recognition, and creating an ongoing, trusting relationship with existing and potential customers.&nbsp;</p></div>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-12 sm-margin-ten-bottom xs-margin-twenty-three-bottom">
+                            <h5 class="margin-eight-bottom text-large alt-font sm-title-small xs-margin-seven-bottom tz-text" style="" data-selector=".tz-text">Stand out from your competition</h5>
+                            <div class="text-medium tz-text" style="" data-selector=".tz-text"><p>Your podcast is the stage for you to claim the rock star status in your industry. It creates an intimate opportunity for your existing and potential buyers to get to know the unique you and what you offer, without you un-scalably trying to chase them down individually.&nbsp;</p></div>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-12 xs-margin-twenty-three-bottom">
+                            <h5 class="margin-eight-bottom text-large alt-font sm-title-small xs-margin-seven-bottom tz-text" style="" data-selector=".tz-text">Boost your industry authority status</h5>
+                            <div class="text-medium tz-text" style="" data-selector=".tz-text"><p>Your podcast is your speaking podium without time limit. It helps you attract other speaking, writing and media exposure opportunities. Depending on the format of your podcast, it also helps you build relationship with other top players in your industry and further solidity your authority status.</p></div>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <h5 class="margin-eight-bottom text-large alt-font sm-title-small xs-margin-seven-bottom tz-text" style="" data-selector=".tz-text">Generate consistent, reliable revenue</h5>
+                            <div class="text-medium tz-text margin-fifteen-bottom xs-margin-eleven-bottom" style="" data-selector=".tz-text"><div>Your podcast, if executed right, becomes a powerful, evergreen customer acquisition funnel. It gives customers the opportunity to come to you and get to know the unique value you provide. It helps you build customer relationships in a scalable manner. So that you can grow your business faster and make a bigger impact.</div></div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
