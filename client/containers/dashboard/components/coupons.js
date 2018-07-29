d2b4c0e2ed777a5376dff53fd0e4a8ad6bb20cd2
Natasha Che <natasha@natashache.com>
2018-07-29 12:28:53 -0400

    added trial coupon

--- client/containers/dashboard/components/coupons.js
+++ client/containers/dashboard/components/coupons.js
@@ -36,7 +36,7 @@ export default class Coupons extends Component {
                 minWidth: 200,
               }}
             >
-              <span>Coupon Code</span>
+              <span>Coupon Code (no space)</span>
               <div>
                 <input
                   type="text"
