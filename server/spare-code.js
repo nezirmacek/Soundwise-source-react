9df747268103ba4e85b06d35e71d2793e2987325
Natasha Che <natasha@natashache.com>
2018-07-29 11:49:29 -0400

    pulling from remote

--- server/spare-code.js
+++ server/spare-code.js
@@ -1,3 +1,5 @@
+var stripe_key = require('../config').stripe_key;
+var stripe = require('stripe')(stripe_key);
 var firebase = require("firebase-admin");
 const moment = require("moment");
 var serviceAccount = require("../serviceAccountKey.json");
@@ -7,29 +9,37 @@ firebase.initializeApp({
   databaseURL: 'https://soundwise-a8e6f.firebaseio.com',
 });
 
-// Add subscribed soundcasts to user
-var userId = 'KAyqJEcOfwhculAwxIHPoQLtbIi2';
-var soundcastsIncluded = [
-  '1531419211997s',
-  '1531441638240s',
-  '1531459034687s',
-  '1531496828981s',
-  '1531502612113s'];
-var subscribedObj = {
-  billingCycle: 'one time',
-  subscribed: true,
-  date_subscribed: 1531755224,
-  current_period_end: 4638902400
-};
+// retrieve stripe product
+stripe.products.retrieve(
+  "prod_D3nYV0LbBizW8s",{stripe_account: 'acct_1Bdla1BEkT8zqJaI'}, function(err, product) {
+    if(err) console.log(err);
+    console.log(product);
+  }
+);
 
-soundcastsIncluded.forEach(soundcastId => {
-  firebase.database().ref(`users/${userId}/soundcasts/${soundcastId}`)
-  .set(subscribedObj);
-  firebase.database().ref(`soundcasts/${soundcastId}/subscribed/${userId}`)
-  .set('1531755224');
-});
-firebase.database().ref(`soundcasts/1531504770898s/subscribed/${userId}`)
-.set('1531755224');
+// Add subscribed soundcasts to user
+// var userId = 'KAyqJEcOfwhculAwxIHPoQLtbIi2';
+// var soundcastsIncluded = [
+//   '1531419211997s',
+//   '1531441638240s',
+//   '1531459034687s',
+//   '1531496828981s',
+//   '1531502612113s'];
+// var subscribedObj = {
+//   billingCycle: 'one time',
+//   subscribed: true,
+//   date_subscribed: 1531755224,
+//   current_period_end: 4638902400
+// };
+
+// soundcastsIncluded.forEach(soundcastId => {
+//   firebase.database().ref(`users/${userId}/soundcasts/${soundcastId}`)
+//   .set(subscribedObj);
+//   firebase.database().ref(`soundcasts/${soundcastId}/subscribed/${userId}`)
+//   .set('1531755224');
+// });
+// firebase.database().ref(`soundcasts/1531504770898s/subscribed/${userId}`)
+// .set('1531755224');
 
 
 // Compile iTunes category ID list
