972233dc0d3326e2d7c2c14c229c11421a5142c9
IvanM <ivan.malyshev.376@gmail.com>
2018-07-30 05:19:54 +0300

    pull 63 number_of_times_a_coupon_is_used merge

--- client/containers/dashboard/components/settings.js
+++ client/containers/dashboard/components/settings.js
@@ -463,6 +463,11 @@ export default class Profile extends Component {
             </Link>
           </li>
           <li role="presentation">
+            <Link to="/dashboard/publisher/promotions">
+              <span style={{fontSize: 15, fontWeight: 600}}>Promotions</span>
+            </Link>
+          </li>
+          <li role="presentation">
             <Link to="/dashboard/publisher/settings">
               <span style={{fontSize: 15, fontWeight: 600}}>Settings</span>
             </Link>
