72d67aba2e75d97cd0205237b6250268dd8ba21b
eredinbg <eredinbg@gmail.com>
2018-08-09 11:18:10 +0300

    Remove STAGING_ENV var

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,7 @@ module.exports = {
         NODE_ENV: 'dev',
       },
       env_staging: {
-        NODE_ENV: 'dev',
-        // it's moved to a separate env variable to not break legacy code
-        STAGING_ENV: true,
+        NODE_ENV: 'staging',
       },
       env_production: {
         NODE_ENV: 'production',
