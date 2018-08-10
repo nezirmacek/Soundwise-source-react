7bd715590470199ec8913fa13e9ef006e638e6bb
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-10 18:18:34 +0300

    Fix pagination

--- server/scripts/syncPsql.js
+++ server/scripts/syncPsql.js
@@ -5,21 +5,62 @@ var moment = require('moment');
 var firebase = require('firebase-admin');
 const database = require('../../database/index');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const syncSoundcasts = async () => {
-  firebase
+  const pageSize = 10;
+
+  let next = true;
+  let startId = '1502366385413s';
+
+  const lastId = await firebase
     .database()
     .ref('soundcasts')
-    .limitToFirst(10)
+    .orderByKey()
+    .limitToLast(1)
     .once('value')
-    .then(snaps => {
-      snaps.forEach(snap => {
-        console.log('soundcasts', snap.key);
+    .then(snap => Object.keys(snap.val())[0]);
+
+  while (next) {
+    await firebase
+      .database()
+      .ref('soundcasts')
+      .orderByKey()
+      .startAt(startId)
+      .limitToFirst(pageSize)
+      .once('value')
+      .then(snaps => {
+        const keys = Object.keys(snaps.val());
+        keys.forEach((key, i) => {
+          next = key !== lastId;
+          if (i === 0) {
+            return;
+          } else if (i === keys.length - 1) {
+            startId = key;
+          }
+          soundcastObj = {};
+          database.Soundcast.findOne({
+            where: { soundcastId: key },
+          })
+            .then(soundcastData => {
+              if (soundcastData) {
+                return soundcastData.update(soundcastObj);
+              } else {
+                return database.Soundcast.create(soundcastObj);
+              }
+            })
+            .catch(err => logInFile(`soundcastId: ${key}\n Err: ${err}`));
+        });
       });
-    });
-  const text = 'testtexttesttexttesttext\n\ntesttexttesttext\ntexttext';
-  fs.writeFile('2pac.txt', text, err => {
+  }
+};
+
+const logInFile = text => {
+  fs.writeFile('logErr.txt', text, err => {
     if (err) throw err;
-    console.log('Text saved!');
+    console.log('Save log');
   });
 };
 
