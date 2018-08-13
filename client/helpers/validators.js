81e077c42ff31b63b698f62d81b33222f09f5043
IvanM <ivan.malyshev.376@gmail.com>
2018-08-13 23:18:05 +0300

    #75 email validator fix

--- client/helpers/validators.js
+++ client/helpers/validators.js
@@ -34,5 +34,5 @@ export function numberValidator(value) {
 
 export function emailValidator(value) {
   const email = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-  return email.test(String(value).toLowerCase());
+  return !email.test(String(value).toLowerCase());
 }
