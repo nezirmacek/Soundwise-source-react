06cbfb0c567b49b9778e1697bd7c38a695f2f15c
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-16 15:02:14 +0300

    Add services for comment and like

---
+++ server/services/comment.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const _ = require('lodash');
+const database = require('../../database/index');
+
+const sendMail = require('../scripts/sendEmails').sendMail;
+
+const sendNotification = require('../scripts/messaging').sendNotification;
+const { getContentPush } = require('../scripts/utils');
+
+const { commentManager } = require('../managers');
+
+const addComment = (req, res) => {
+  database.Comment.create(req.body)
+    .then(data => {
+      const comment = data.dataValues;
+      const commentId = comment.commentId;
+      let fbComment = {
+        userID: comment.userId,
+        content: comment.content,
+        timestamp: comment.timeStamp,
+        parentID: comment.parentId,
+        episodeID: comment.episodeId,
+        soundcastID: comment.soundcastId,
+        announcementID: comment.announcementId,
+      };
+      fbComment = _.pickBy(fbComment, _.identity);
+
+      commentManager
+        .addComment(commentId, fbComment)
+        .then(() => {
+          if (comment.parentId) {
+            commentManager.getUserParentComment(commentId).then(user => {
+              if (user && !!user.token) {
+                user.token.forEach(t =>
+                  sendNotification(t, {
+                    data: { type: '', messageId: '', soundcastId: '' },
+                    notification: { title: 'title', body: '' },
+                  })
+                );
+              }
+              sendMail(fbComment);
+              res.send(data);
+            });
+          }
+        })
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+};
+const editComment = (req, res) => {
+  const comment = req.body;
+  const commentId = req.params.id;
+  let fbComment = {
+    userID: comment.userId,
+    content: comment.content,
+    timestamp: comment.timeStamp,
+    parentID: comment.parentId,
+    episodeID: comment.episodeId,
+    soundcastID: comment.soundcastId,
+    announcementID: comment.announcementId,
+  };
+  fbComment = _.pickBy(fbComment, _.identity);
+  database.Comment.update(comment, {
+    where: { commentId: commentId },
+  })
+    .then(data => {
+      commentManager
+        .addComment(commentId, fbComment)
+        .then(() => res.send(data));
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+};
+
+const deleteComment = (req, res) => {
+  const commentId = req.params.id;
+  database.Comment.find({ where: { commentId } })
+    .then(data => {
+      const comment = data.dataValues;
+      database.Comment.destroy({
+        where: { commentId },
+      }).then(count => {
+        commentManager
+          .removeComment(commentId, comment)
+          .then(() => res.send({ count }));
+      });
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+module.exports = {
+  addComment,
+  editComment,
+  deleteComment,
+};
---
+++ server/services/like.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const _ = require('lodash');
+const database = require('../../database/index');
+
+const sendNotification = require('../scripts/messaging').sendNotification;
+const getContentPush = require('../scripts/utils').getContentPush;
+
+const { commentManager, likeManager } = require('../managers');
+
+const likesCount = like => {
+  const path = like.announcementId
+    ? `messages/${like.announcementId}/likesCount`
+    : like.episodeId
+      ? `episodes/${like.episodeId}/likesCount`
+      : `comments/${like.commentId}/likesCount`;
+  return database.Like.count({ where: { episodeId: like.episodeId } }).then(
+    count => likeManager.setLikesCount(path, count)
+  );
+};
+
+const addLike = (req, res) => {
+  console.log('add\n');
+  database.Like.create(req.body)
+    .then(data => {
+      const like = data.dataValues;
+      const likeId = like.likeId;
+      const fullName = req.body.fullName;
+      let fbLike = {
+        likeID: like.likeId,
+        userID: like.userId,
+        timestamp: like.timeStamp,
+        episodeID: like.episodeId,
+        commentID: like.commentId,
+        soundcastID: like.soundcastId,
+        announcementID: like.announcementId,
+      };
+      fbLike = _.pickBy(fbLike, _.identity);
+
+      likeManager.addLike(likeId, fbLike).then(() => {
+        if (like.commentId) {
+          commentManager.getUserParentComment(like.commentId).then(user => {
+            if (user && !!user.token) {
+              user.token.forEach(t =>
+                sendNotification(t, getContentPush(like))
+              );
+            }
+          });
+        }
+        likeManager.setFullName(like, fullName).then(() => {
+          likesCount(like).then(() => res.send(data));
+        });
+      });
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+const deleteLike = (req, res) => {
+  const likeId = req.params.id;
+  console.log('delete\n');
+  database.Like.find({ where: { likeId } })
+    .then(data => {
+      const like = data.dataValues;
+      database.Like.destroy({ where: { likeId } }).then(() => {
+        likeManager.removeLike(likeId, like).then(() => {
+          if (!!like.episodeId) {
+            // set name for lastLiked
+            database.Like.findAll({
+              where: { episodeId: like.episodeId },
+            }).then(likes => {
+              if (likes.length > 0) {
+                let lastLike = _.maxBy(likes, 'timeStamp');
+                likeManager
+                  .setFullNameAfterRemove(lastLike.dataValues.userId, like)
+                  .then(fullName => {
+                    likesCount(like);
+                    res.send({ fullName });
+                  });
+              }
+            });
+          } else if (!!like.announcementId) {
+            database.Like.findAll({
+              where: { announcementId: like.announcementId },
+            }).then(likes => {
+              if (likes.length > 0) {
+                let lastLike = _.maxBy(likes, 'timeStamp');
+                likeManager
+                  .setFullNameAfterRemove(lastLike.dataValues.userId, like)
+                  .then(fullName => {
+                    likesCount(like);
+                    res.send({ fullName });
+                  });
+              }
+            });
+          }
+        });
+      });
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+module.exports = {
+  addLike,
+  deleteLike,
+};
