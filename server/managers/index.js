96a4a9631ae954b990cd346975badf33969c71d8
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-16 15:01:46 +0300

    Add managers for like and comment

--- server/managers/index.js
+++ server/managers/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const commonManager = require('./common');
+const commentManager = require('./comment');
+const likeManager = require('./like');
 const invitationManager = require('./invitation');
 const mailingManager = require('./mailing');
 const publisherManager = require('./publisher');
@@ -9,6 +11,8 @@ const userManager = require('./user');
 
 module.exports = {
   commonManager,
+  commentManager,
+  likeManager,
   invitationManager,
   mailingManager,
   publisherManager,
---
+++ server/managers/like.js
@@ -0,0 +1,107 @@
+'use strict';
+const moment = require('moment');
+const firebase = require('firebase-admin');
+const _ = require('lodash');
+
+const getById = id =>
+  firebase
+    .database()
+    .ref(`likes/${id}`)
+    .once('value')
+    .then(snapshot => (snapshot.exists() ? snapshot.val() : null));
+
+const addLike = (id, like) =>
+  firebase
+    .database()
+    .ref(`likes/${id}`)
+    .set(like)
+    .then(() =>
+      firebase
+        .database()
+        .ref(
+          like.announcementID
+            ? `messages/${like.announcementID}/likes/${id}`
+            : like.episodeId
+              ? `episodes/${like.episodeID}/likes/${id}`
+              : `comments/${like.commentID}/likes/${id}`
+        )
+        .set(moment().format('X'))
+    );
+
+const removeLike = (id, like) =>
+  firebase
+    .database()
+    .ref(
+      like.announcementId
+        ? `messages/${like.announcementId}/likes/${id}`
+        : like.episodeId
+          ? `episodes/${like.episodeId}/likes/${id}`
+          : `comments/${like.commentId}/likes/${id}`
+    )
+    .remove()
+    .then(() =>
+      firebase
+        .database()
+        .ref(`likes/${id}`)
+        .remove()
+    );
+
+const setLikesCount = (path, count) =>
+  firebase
+    .database()
+    .ref(path)
+    .set(count);
+
+const setFullName = (like, fullName) => {
+  if (fullName !== undefined) {
+    if (like.episodeId !== undefined) {
+      return firebase
+        .database()
+        .ref(`episodes/${like.episodeId}/lastLiked`)
+        .set(fullName);
+    } else if (like.announcementId !== undefined) {
+      return firebase
+        .database()
+        .ref(`messages/${like.announcementId}/lastLiked`)
+        .set(fullName);
+    }
+  }
+};
+
+const setFullNameAfterRemove = (userId, like) =>
+  firebase
+    .database()
+    .ref(`users/${userId}`)
+    .once('value')
+    .then(snapshot => {
+      if (snapshot.val()) {
+        const user = snapshot.val();
+        const fullName = `${user.firstName} ${user.lastName}`;
+        if (like.episodeId !== undefined) {
+          console.log(fullName);
+          return firebase
+            .database()
+            .ref(`episodes/${like.episodeId}/lastLiked`)
+            .set(fullName)
+            .then(() => fullName);
+        } else if (like.announcementId !== undefined) {
+          console.log(fullName);
+          return firebase
+            .database()
+            .ref(`messages/${like.announcementId}/lastLiked`)
+            .set(fullName)
+            .then(() => fullName);
+        }
+      } else {
+        return false;
+      }
+    });
+
+module.exports = {
+  getById,
+  addLike,
+  removeLike,
+  setFullName,
+  setLikesCount,
+  setFullNameAfterRemove,
+};
