83c4319da8b64a2d79e7680926f5f70a1b5618f3
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-22 13:45:11 +0300

    Add repo for comments and messages, also refactor comment services

---
+++ server/repositories/comment.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const database = require('../../database');
+
+const get = commentId =>
+  database.Comment.findOne({where: {commentId}}).then(
+    data => (data ? data.dataValues : null)
+  );
+
+const create = comment =>
+  database.Comment.create(comment).then(
+    data => (data ? data.dataValues : null)
+  );
+
+const update = (comment, id) =>
+  database.Comment.update(comment, {where: {commentId: id}});
+
+const destroy = commentId => database.Comment.destroy({where: {commentId}});
+
+const count = where => database.Comment.count({where});
+
+module.exports = {
+  get,
+  create,
+  update,
+  destroy,
+  count,
+};
--- server/repositories/index.js
+++ server/repositories/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const announcementRepository = require('./announcement');
 const soundcastRepository = require('./soundcast');
+const commentRepository = require('./comment');
 const userRepository = require('./user');
 
 module.exports = {
+  announcementRepository,
   soundcastRepository,
+  commentRepository,
   userRepository,
 };
