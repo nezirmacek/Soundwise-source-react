5550d3fda8f89d5e8fa11f1d053945c9d2831050
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-25 18:48:55 +0300

    Fix impot Axios

--- client/containers/dashboard/components/invite_subscribers_modal.js
+++ client/containers/dashboard/components/invite_subscribers_modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import Axios from 'axios';
 import firebase from 'firebase';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
