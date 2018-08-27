3ef4c96b3d9181261c432bd1723f7afa00b9d081
VladislavNovikov <vladislav4565478@gmail.com>
2018-08-27 17:53:38 +0300

    Fix child comment in fixing script

--- server/syncComments.js
+++ server/syncComments.js
@@ -3,13 +3,14 @@
 const fs = require('fs');
 const firebase = require('firebase-admin');
 const database = require('../database');
+const moment = require('moment');
 var serviceAccount = require('../serviceAccountKey');
 
 const LOG_ERR = 'logErrsComments.txt';
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: 'https://soundwise-a8e6f.firebaseio.com',
+  databaseURL: 'https://soundwise-testbase.firebaseio.com',
 });
 
 const syncMessages = async () => {
@@ -21,27 +22,89 @@ const syncMessages = async () => {
   const keys = Object.keys(comments);
   for (const key of keys) {
     const fbComment = comments[key];
-    const comment = {
-      commentId: key,
-      userId: fbComment.userID || fbComment.userId || null,
-      content: fbComment.content,
-      parentId: fbComment.parentId || fbComment.parentID || null,
-      episodeId: fbComment.episodeID || null,
-      soundcastId: fbComment.soundcastId || fbComment.soundcastID || null,
-      announcementId:
-        fbComment.announcementId || fbComment.announcementID || null,
-      timeStamp: fbComment.timestamp,
-    };
-    await database.Comment.create(comment)
-      .then(data => console.log(data.dataValues))
-      .catch(e => {
-        console.log(e);
-        logInFile(`ID: ${key}\nERROR: ${e}\n\n`);
-      });
+    if (fbComment) {
+      if (fbComment.children) {
+        const childKeys = Object.keys(fbComment.children);
+        for (const childKey of childKeys) {
+          const childComment = (await firebase
+            .database()
+            .ref(`comments/${childKey}`)
+            .once('value')).val();
+          if (childComment) {
+            const comment = getCommentForPsql(childKey, childComment, key);
+            const isExist = await database.Comment.findById(comment.commentId);
+            console.log('isExist:', !!isExist);
+            if (!!isExist) {
+              console.log('update');
+              console.log('comment', comment);
+              await database.Comment.update(comment, {
+                where: { commentId: childKey },
+              })
+                .then(data => console.log(data + '\n'))
+                .catch(e => {
+                  console.log('ERROR' + e + '\n');
+                  logInFile(`ID: ${key}\nERROR: ${e}\n\n`);
+                });
+            } else {
+              if (comment) {
+                console.log('create');
+                console.log('comment', comment);
+                await database.Comment.create(comment)
+                  .then(data => console.log('data', data.dataValues + '\n'))
+                  .catch(e => {
+                    console.log('ERROR' + e + '\n\n');
+                    logInFile(`ID: ${key}\nERROR: ${e}\n`);
+                  });
+              }
+            }
+          }
+        }
+      } else {
+        const comment = getCommentForPsql(key, fbComment);
+        const isExist = await database.Comment.findById(comment.commentId);
+        console.log('isExist:', !!isExist);
+        if (!!isExist) {
+          console.log('update');
+          console.log('comment', comment);
+          await database.Comment.update(comment, { where: { commentId: key } })
+            .then(data => console.log(data + '\n'))
+            .catch(e => {
+              console.log('ERROR' + e + '\n');
+              logInFile(`ID: ${key}\nERROR: ${e}\n\n`);
+            });
+        } else {
+          if (comment) {
+            console.log('create');
+            console.log('comment', comment);
+            await database.Comment.create(comment)
+              .then(data => console.log('data', data.dataValues + '\n'))
+              .catch(e => {
+                console.log('ERROR' + e + '\n');
+                logInFile(`ID: ${key}\nERROR: ${e}\n\n`);
+              });
+          }
+        }
+      }
+    }
   }
   console.log('finish');
 };
 
+const getCommentForPsql = (key, fbComment, parent) => {
+  const comment = {
+    commentId: key,
+    userId: fbComment.userID || fbComment.userId || null,
+    content: fbComment.content,
+    parentId: parent ? parent : fbComment.parentID || fbComment.parentId,
+    episodeId: fbComment.episodeID || null,
+    soundcastId: fbComment.soundcastId || fbComment.soundcastID || null,
+    announcementId:
+      fbComment.announcementId || fbComment.announcementID || null,
+    timeStamp: fbComment.timestamp || fbComment.timeStamp || null,
+    createdAt: moment.unix(fbComment.timestamp).format('YYYY-MM-DD HH:mm:ss Z'),
+  };
+  return comment;
+};
 const logInFile = text => {
   fs.appendFile(LOG_ERR, text, err => {
     if (err) throw err;
