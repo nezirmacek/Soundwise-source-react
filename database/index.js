cd7c9accc86374f4abdaba14bfc8d4fec3dc24be
IvanM <ivan.malyshev.376@gmail.com>
2018-09-29 15:13:41 +0300

    #125 Sequelize warning fix

--- database/index.js
+++ database/index.js
@@ -1,4 +1,41 @@
-var Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col,
+};
 var db;
 
 if (process.env.DATABASE_URL) {
@@ -9,6 +46,7 @@ if (process.env.DATABASE_URL) {
     port: match[4],
     host: match[3],
     logging: false,
+    operatorsAliases,
     dialectOptions: { ssl: true },
   });
 } else {
@@ -16,6 +54,7 @@ if (process.env.DATABASE_URL) {
     dialect: 'postgres',
     port: 5432,
     logging: false,
+    operatorsAliases,
     dialectOptions: { ssl: true },
   });
 }
